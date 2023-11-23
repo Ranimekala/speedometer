651032b39548d2109a73670cd7dce73b6808fc0e
reshma <krishnaveniranimekala@gamail.com>
2023-11-23 14:31:49 +0000

    speedometer solution

---
+++ src/components/Speedometer/index.js
@@ -0,0 +1,13 @@
+// Write your code here
+
+import {Component} from 'react'
+
+class Speedometer extends Component {
+  render() {
+    ;<div className="container">
+      <h1>"Speedometer"</h1>
+    </div>
+  }
+}
+
+export default Speedometer
